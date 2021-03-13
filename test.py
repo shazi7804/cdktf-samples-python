db6d9c408e2475eb45e8c837205241dcccfae81d
shazi7804 <shazi7804@gmail.com>
2021-03-14 00:44:26 +0800

    delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-        manage_policies={
-            "ssm": 'arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM',
-            "s3": 'arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess'}
-            
-        for policy, arn in manage_policies.items():
-            print(policy, arn)
